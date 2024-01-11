0ebc036a82f10848b0dddeb627048a6ef2579d71
Damian Perera <damianperera@outlook.com>
2024-01-11 22:59:42 +0100

    added drone map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import SearchAppBar from './components/SearchAppBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://images.tovisit.eu/flymydrone/logo/png/logo-no-background.png" className="App-logo" alt="logo" />
-      </header>
+      <SearchAppBar />
+      <div id="map" className="DroneMap">
+          <iframe className="ResponsiveIframe" src="https://dronemaps24.org/?lang=en" title="Drone Map" allow="geolocation 'src';"></iframe>
+        </div>
     </div>
   );
 }
