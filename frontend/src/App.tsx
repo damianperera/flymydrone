cc02b461411af6f968b4c3e8aeeae52b2de433eb
Damian Perera <damianperera@outlook.com>
2024-01-11 23:37:55 +0100

    added top bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import SearchAppBar from './components/SearchAppBar';
+import DroneMap from './components/DroneMap';
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
-      <div id="map" className="DroneMap">
-          <iframe className="ResponsiveIframe" src="https://dronemaps24.org/?lang=en" title="Drone Map" allow="geolocation 'src';"></iframe>
-        </div>
+      <DroneMap />
     </div>
   );
 }
---
+++ frontend/src/components/DroneMap.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DroneMap = () => {
+
+  return (
+    <div className='DroneMap'>
+      <iframe className='ResponsiveIframe' src='https://dronemaps24.org/?lang=en' title='Drone Map' allow="geolocation 'src';"></iframe>
+    </div>
+  );
+};
+
+export default DroneMap;
